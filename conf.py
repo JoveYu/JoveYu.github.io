90a7f45f6f76e59d87924f36b150257b83578626
yushijun <yushijun@qfpay.com>
2015-01-05 18:11:54 +0800

    添加baseurl

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ SITE_URL = "http://jovesky.com"
 # This is the URL where nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://jovesky.com"
-#BASE_URL = "http://jovesky.com/"
+BASE_URL = "http://jovesky.com/"
 BLOG_EMAIL = "yushijun110@gmail.com"
 BLOG_DESCRIPTION = "I love what I do"  # (translatable)
 
